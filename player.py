51c3e2e2c20d25cfffc87dfcf003145c8b06e281
Tedwards <108353463+Tedwards22890@users.noreply.github.com>
2022-07-26 16:00:08 -0400

    Fixed spelling errors and users able to trick program into endlessly repeating

--- player.py
+++ player.py
@@ -7,14 +7,7 @@ class Player:
         self.attacks = [
             {'number':'0','name':'Rock'},
             {'number':'1','name':'Paper'},
-            {'number':'2','name':'Scizzors'},
+            {'number':'2','name':'Scissors'},
             {'number':'3','name':'Lizard'},
             {'number':'4','name':'Spock'}
         ]
-
-    def clear_gesture(self):
-        self.gesture=None
-    
-
-    
-
